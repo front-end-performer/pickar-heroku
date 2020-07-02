4127e825513db3c63424d0c41542a7e5d5e11cd6
Oleksii Sodolinskyi <sodolinskyi@gmail.com>
2020-07-02 20:05:23 +0200

    fixed 422 error

---
+++ backend/public/precache-manifest.1c8cd0c943940665413235439b9bc578.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab25883c5b3a8bfedadc08dfb0bfcfd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c82f6be6e78c6d6858c",
+    "url": "/static/css/5.6481878c.chunk.css"
+  },
+  {
+    "revision": "f999777fcf1800337f36",
+    "url": "/static/css/main.c8dd1876.chunk.css"
+  },
+  {
+    "revision": "01249d7719264b213673",
+    "url": "/static/js/0.2f5dc2e5.chunk.js"
+  },
+  {
+    "revision": "a8a90041304c354727b1",
+    "url": "/static/js/3.2b5b14c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/3.2b5b14c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb51000c3afe49a0dfcf",
+    "url": "/static/js/4.212edbb0.chunk.js"
+  },
+  {
+    "revision": "0c82f6be6e78c6d6858c",
+    "url": "/static/js/5.109498c2.chunk.js"
+  },
+  {
+    "revision": "f2a40e095ebc7d90d92b",
+    "url": "/static/js/6.f39796fb.chunk.js"
+  },
+  {
+    "revision": "3cfd5f380d85874231f9",
+    "url": "/static/js/7.f4982ebe.chunk.js"
+  },
+  {
+    "revision": "f999777fcf1800337f36",
+    "url": "/static/js/main.2e384de5.chunk.js"
+  },
+  {
+    "revision": "9b796b4b91381858768d",
+    "url": "/static/js/runtime-main.c5c5a5f4.js"
+  }
+]);
--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b69349231abe735ea1c730e75360265.js"
+  "/precache-manifest.1c8cd0c943940665413235439b9bc578.js"
 );
 
 self.addEventListener('message', (event) => {
