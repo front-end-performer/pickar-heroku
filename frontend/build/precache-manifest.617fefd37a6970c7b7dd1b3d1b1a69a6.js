dcf8edd8287be26e77a374baf00aaa7ccf794969
Oleksii Sodolinskyi <sodolinskyi@gmail.com>
2020-07-02 12:33:45 +0200

    delete build up files

---
+++ frontend/build/precache-manifest.617fefd37a6970c7b7dd1b3d1b1a69a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd7bc5639cd7059c4d38f674f6b4f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b25cb5264dc9ad72db03",
+    "url": "/static/css/5.6481878c.chunk.css"
+  },
+  {
+    "revision": "f999777fcf1800337f36",
+    "url": "/static/css/main.c8dd1876.chunk.css"
+  },
+  {
+    "revision": "01249d7719264b213673",
+    "url": "/static/js/0.2f5dc2e5.chunk.js"
+  },
+  {
+    "revision": "a8a90041304c354727b1",
+    "url": "/static/js/3.2b5b14c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/3.2b5b14c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb51000c3afe49a0dfcf",
+    "url": "/static/js/4.212edbb0.chunk.js"
+  },
+  {
+    "revision": "b25cb5264dc9ad72db03",
+    "url": "/static/js/5.2bc43e44.chunk.js"
+  },
+  {
+    "revision": "0335a97eac5d813a95df",
+    "url": "/static/js/6.93d03c3d.chunk.js"
+  },
+  {
+    "revision": "3cfd5f380d85874231f9",
+    "url": "/static/js/7.f4982ebe.chunk.js"
+  },
+  {
+    "revision": "f999777fcf1800337f36",
+    "url": "/static/js/main.2e384de5.chunk.js"
+  },
+  {
+    "revision": "49da15f8bb9ae8462e6a",
+    "url": "/static/js/runtime-main.4add2aa1.js"
+  }
+]);
