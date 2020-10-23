a5e976cca9b0af7dc1957393553360170bb47e31
Oleksii Sodolinskyi <admin@Oleksiis-MBP.fritz.box>
2020-10-23 19:25:23 +0200

    changed db

--- backend/app.js
+++ backend/app.js
@@ -48,8 +48,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-pickar.b7z3u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-    // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-pickar-b7z3u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@pickur-cluster.b7z3u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
